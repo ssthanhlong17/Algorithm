74fd8e7d729d58e9d3ed9eaab5f1945342490524
PhamLong <long1792003@gmail.com>
2026-04-14 15:40:11 +0700

    Update LeetCode

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,6 +1,6 @@
 bool isPerfectSquare(int num) {
     if(num == 1) return true;
-    for(long i=0; i<num; i++){  
+    for(long i=0; i<num/2; i++){  
         
         if(i*i == num) return true;
     }
